0b172ce88281126034cce4a36002d0cf2bca3333
Balla-Schottner Bence <hello@ballaschottner.com>
2021-02-06 14:38:22 +0100

    sokadik

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,18 +4,32 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const addBtn = document.getElementById("addBtn");
     const list = document.getElementById("todo-list");
-    const todoValue = document.getElementById("todo_input")
+    const todoValue = document.getElementById("todo_input");
+    const inputValidation = document.getElementById("validation");
+    const node = document.createElement("li")
 
 
     addBtn.addEventListener("click", function () {
 
         const todoTxt = todoValue.value;
         const liElem = document.createElement("li");
+        const remove = document.createElement("button");
+
+        node.innterHMTL = `<p>${todoTxt} <button>X</button></p>`;
 
         liElem.textContent = todoTxt;
+        remove.textContent = "delete";
 
-        list.prepend(liElem);
+        list.append(liElem);
     });
+
+    todoValue.addEventListener("keyup", (event) => {
+        console.log(todoValue);
+        console.log(event);
+        if(todoValue.length >= 6) {
+            inputValidation.style.display = "none";
+        }
+    })
 
     list.addEventListener("click", event => {
         console.log("item", event);
