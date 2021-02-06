b021f6869f891113d1a6c943eae1d8ec914059cd
Balla-Schottner Bence <hello@ballaschottner.com>
2021-02-06 10:28:49 +0100

    MZ-02

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,2 +1,32 @@
-//can be deleted
-console.log("Console log works!");
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const addBtn = document.getElementById("addBtn");
+    const list = document.getElementById("todo-list");
+    const todoValue = document.getElementById("todo_input")
+
+
+    addBtn.addEventListener("click", function () {
+
+        const todoTxt = todoValue.value;
+        const liElem = document.createElement("li");
+
+        liElem.textContent = todoTxt;
+
+        list.prepend(liElem);
+    });
+
+    list.addEventListener("click", event => {
+        console.log("item", event);
+
+        if(event.target.nodeName === "LI") {
+            event.target.remove();
+        }
+    })
+
+    document.body.addEventListener("click", event => {
+        console.log("body", event);
+    })
+
+});
